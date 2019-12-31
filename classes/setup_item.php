553644781312cec0078feff4d40f0ff7ef989b6e
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2019-12-31 11:48:41 +1100

    Add set up item class

---
+++ classes/setup_item.php
@@ -0,0 +1,150 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Set up item instance.
+ *
+ * @package    tool_blocksmanager
+ * @author     Dmitrii Metelkin <dmitriim@catalyst-au.net>
+ * @copyright  Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_blocksmanager;
+
+defined('MOODLE_INTERNAL') || die();
+
+class setup_item {
+
+    /**
+     * Data delimiter.
+     */
+    const DATA_DELIMITER = ':';
+
+    /**
+     * Region it put the block to.
+     * @var string
+     */
+    protected $region;
+
+    /**
+     * A list of categories to apply set up item.
+     * @var array
+     */
+    protected $categories;
+
+    /**
+     * A name of the block.
+     * @var string
+     */
+    protected $blockname;
+
+    /**
+     * Block weight.
+     * @var int
+     */
+    protected $weight = 0;
+
+    /**
+     * Block visibility.
+     * @var int
+     */
+    protected $visible = 1;
+
+    /**
+     * Constructor.
+     *
+     * @param string $data Incoming data.
+     *
+     * @throws \tool_blocksmanager\invalid_setup_item_exception
+     */
+    public function __construct(string $data) {
+        // Should be at least 3 values.
+        if (substr_count($data, self::DATA_DELIMITER) < 2) {
+            throw new invalid_setup_item_exception('Incorrect data: not all required fields provided');
+        }
+
+        $values = explode(':', trim($data));
+
+        $this->region = trim($values[0]);
+
+        if (!empty($values[1])) {
+            $catids = explode(',', trim($values[1]));
+            foreach ($catids as $catid) {
+                if (!is_numeric($catid)) {
+                    throw new invalid_setup_item_exception('Incorrect data: incorrect category id provided');
+                }
+            }
+        }
+
+        $this->categories = helper::get_categories_and_children(trim($values[1]));
+        $this->blockname = trim($values[2]);
+
+        if (isset($values[3]) && is_numeric($values[3])) {
+            $this->weight = trim($values[3]);
+        }
+
+        if (isset($values[4]) && is_numeric($values[4])) {
+            $this->visible = trim($values[4]);
+        }
+    }
+
+    /**
+     * Returns region.
+     *
+     * @return mixed
+     */
+    public function get_region() {
+        return $this->region;
+    }
+
+    /**
+     * Returns categories.
+     *
+     * @return array
+     */
+    public function get_categories() {
+        return $this->categories;
+    }
+
+    /**
+     * Returns block name.
+     *
+     * @return mixed
+     */
+    public function get_blockname() {
+        return $this->blockname;
+    }
+
+    /**
+     * Returns block weight.
+     *
+     * @return int
+     */
+    public function get_weight(): int {
+        return (int) $this->weight;
+    }
+
+    /**
+     * Returns bock visibility.
+     *
+     * @return int
+     */
+    public function get_visible(): int {
+        return (int) $this->visible;
+    }
+
+}
---
+++ tests/phpunit/setup_item_test.php
@@ -0,0 +1,71 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for setup_item class.
+ *
+ * @package     tool_blocksmanager
+ * @copyright   2019 Catalyst IT
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class tool_blocksmanager_setup_item_testcase extends advanced_testcase {
+
+    public function test_delimiter() {
+        $this->assertSame(':', \tool_blocksmanager\setup_item::DATA_DELIMITER);
+    }
+
+    public function test_exception_if_not_all_required_fileds() {
+        $this->expectException('\tool_blocksmanager\invalid_setup_item_exception');
+        $this->expectExceptionMessage('Incorrect data: not all required fields provided');
+
+        $item = new \tool_blocksmanager\setup_item('region:1');
+    }
+
+    public function test_exception_if_incorrect_category_provided() {
+        $this->expectException('\tool_blocksmanager\invalid_setup_item_exception');
+        $this->expectExceptionMessage('Incorrect data: incorrect category id provided');
+
+        $item = new \tool_blocksmanager\setup_item('region:1,category:test');
+    }
+
+    public function test_weight_and_visible_defaults() {
+        $item = new \tool_blocksmanager\setup_item('region:1:test');
+        $this->assertSame(0, $item->get_weight());
+        $this->assertSame(1, $item->get_visible());
+    }
+
+    public function test_categories_are_empty_if_not_found() {
+        $item = new \tool_blocksmanager\setup_item('region:777:test');
+        $this->assertSame([], $item->get_categories());
+    }
+
+    public function test_correct_data() {
+        $category1 = $this->getDataGenerator()->create_category();
+        $category11 = $this->getDataGenerator()->create_category(['parent' => $category1->id]);
+        $category111 = $this->getDataGenerator()->create_category(['parent' => $category11->id]);
+
+        $item = new \tool_blocksmanager\setup_item('region:' .$category1->id . ':test_name:-13:0');
+        $this->assertSame('region', $item->get_region());
+        $this->assertSame('test_name', $item->get_blockname());
+        $this->assertSame([$category1->id, $category11->id, $category111->id], $item->get_categories());
+        $this->assertSame(-13, $item->get_weight());
+        $this->assertSame(0, $item->get_visible());
+    }
+
+}
